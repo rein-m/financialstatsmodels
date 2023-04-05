c1e8626f365ddcf7da5130c0c423aad6f4ffd756
Rein McKelway <95264395+veil00@users.noreply.github.com>
2023-04-04 20:58:41 -0400

    Add files via upload
    The relative strength index (RSI) is a technical indicator used in stock trading analysis to measure the strength of a stock's price action by comparing the magnitude of recent gains to recent losses in an attempt to determine overbought and oversold conditions of an asset.
    The RSI is calculated using a formula that takes into account the average gain and average loss over a specified time period. The resulting value ranges from 0 to 100, with a value of 70 or above indicating an overbought condition and a value of 30 or below indicating an oversold condition.

---
+++ RsiCalculator.java
@@ -0,0 +1,58 @@
+import java.util.List;
+import java.util.*;
+
+public class RsiCalculator {
+//will only return actual rsi values for the last 3 of nine timepoints because of the way RSI is calculated.
+    public static double[] getRSI(double[] prices, int n) {
+        double[] deltas = new double[prices.length - 1];
+
+        for (int i = 0; i < deltas.length; i++) {
+            deltas[i] = prices[i+1] - prices[i];
+        }
+
+        double[] seed = Arrays.copyOfRange(deltas, 0, n+1);
+        double up = 0, down = 0;
+
+        for (int i = 0; i < seed.length; i++) {
+            if (seed[i] >= 0) {
+                up += seed[i];
+            } else {
+                down += Math.abs(seed[i]);
+            }
+        }
+
+        up /= n;
+        down /= n;
+
+        double[] rs = new double[prices.length];
+        double[] rsi = new double[prices.length];
+        rs[n] = up/down;
+        rsi[n] = 100.0 - 100.0 / (1.0 + rs[n]);
+
+        for (int i = n+1; i < prices.length; i++) {
+            double delta = deltas[i-1];
+
+            if (delta >= 0) {
+                up = (up*(n-1) + delta)/n;
+                down = (down*(n-1))/n;
+            } else {
+                down = (down*(n-1) + Math.abs(delta))/n;
+                up = (up*(n-1))/n;
+            }
+
+            rs[i] = up/down;
+            rsi[i] = 100.0 - 100.0 / (1.0 + rs[i]);
+        }
+
+        return rsi;
+    }
+
+    public static void main(String[] args) {
+        double[] prices = {10.0, 11.0, 12.0, 11.5, 11.0, 10.5, 11.0, 11.5, 12.0, 13.0};
+        double[] rsi = getRSI(prices, 7);
+
+        for (int i = 0; i < rsi.length; i++) {
+            System.out.println("RSI[" + i + "] = " + rsi[i]);
+        }
+    }
+}
