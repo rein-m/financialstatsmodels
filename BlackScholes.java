cc14eeea4c38d2d82ec5c8f9943c89b99d2753b2
Rein McKelway <95264395+veil00@users.noreply.github.com>
2023-04-25 17:10:40 -0400

    Add files via upload

---
+++ BlackScholes.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class BlackScholes {
+    //please note that the CDF and erf methods are used to calculate the cumulative distribution function and error function, respectively, which are required for the Black-Scholes calculation. These methods are implemented using the approximation formula provided on Wikipedia.
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the current stock price: ");
+        double S = input.nextDouble();
+
+        System.out.print("Enter the strike price: ");
+        double K = input.nextDouble();
+
+        System.out.print("Enter the risk-free interest rate (as a decimal): ");
+        double r = input.nextDouble();
+
+        System.out.print("Enter the time to expiration (in years): ");
+        double T = input.nextDouble();
+
+        System.out.print("Enter the volatility (as a decimal): ");
+        double sigma = input.nextDouble();
+
+        double d1 = (Math.log(S/K) + (r + sigma*sigma/2)*T) / (sigma*Math.sqrt(T));
+        double d2 = d1 - sigma*Math.sqrt(T);
+
+        double callPrice = S*CDF(d1) - K*Math.exp(-r*T)*CDF(d2);
+        double putPrice = K*Math.exp(-r*T)*CDF(-d2) - S*CDF(-d1);
+
+        System.out.println("Call option price: %.2f%n", callPrice);
+        System.out.println("Put option price: %.2f%n", putPrice);
+    }
+
+    public static double CDF(double x) {
+        return 0.5*(1.0 + erf(x/Math.sqrt(2.0)));
+    }
+
+    public static double erf(double z) {
+        double t = 1.0 / (1.0 + 0.5*Math.abs(z));
+
+        double ans = 1 - t*Math.exp(-z*z - 1.26551223 +
+                            t*(1.00002368 +
+                            t*(0.37409196 +
+                            t*(0.09678418 +
+                            t*(-0.18628806 +
+                            t*(0.27886807 +
+                            t*(-1.13520398 +
+                            t*(1.48851587 +
+                            t*(-0.82215223 +
+                            t*0.17087277)))))))));
+
+        if (z >= 0.0) {
+            return ans;
+        } else {
+            return -ans;
+        }
+    }
+}
